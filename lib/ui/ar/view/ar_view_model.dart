4a6707ec9b24ba4fb7fbf0c71865699bf649b07a
ricarth.lima <ricarth.lima@gmail.com>
2025-02-27 15:29:54 -0300

    detectando clique no plano

--- lib/ui/ar/view/ar_view_model.dart
+++ lib/ui/ar/view/ar_view_model.dart
@@ -1,8 +1,11 @@
+import 'package:ar_flutter_plugin_2/datatypes/hittest_result_types.dart';
 import 'package:ar_flutter_plugin_2/datatypes/node_types.dart';
 import 'package:ar_flutter_plugin_2/managers/ar_anchor_manager.dart';
 import 'package:ar_flutter_plugin_2/managers/ar_location_manager.dart';
 import 'package:ar_flutter_plugin_2/managers/ar_object_manager.dart';
 import 'package:ar_flutter_plugin_2/managers/ar_session_manager.dart';
+import 'package:ar_flutter_plugin_2/models/ar_anchor.dart';
+import 'package:ar_flutter_plugin_2/models/ar_hittest_result.dart';
 import 'package:ar_flutter_plugin_2/models/ar_node.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_ar_android_study/domain/models/furniture.dart';
@@ -10,6 +13,7 @@ import 'package:vector_math/vector_math_64.dart';
 
 class ARViewModel extends ChangeNotifier {
   late ARObjectManager _objectManager;
+  late ARAnchorManager _anchorManager;
 
   bool isLoading = false;
 
@@ -24,9 +28,15 @@ class ARViewModel extends ChangeNotifier {
       showAnimatedGuide: true,
       showWorldOrigin: true,
       showPlanes: true,
+
+      //handles
+      handleTaps: true,
     );
 
     _objectManager = objectManager;
+    _anchorManager = anchorManager; // Isso vem depois por necessidade
+
+    sessionManager.onPlaneOrPointTap = onPlaneTapped;
   }
 
   Future<void> onPlaceObjectClicked(Furniture furniture) async {
@@ -42,6 +52,20 @@ class ARViewModel extends ChangeNotifier {
     toggleLoading();
     await _objectManager.addNode(newNode);
     toggleLoading();
+  }
+
+  Future<void> onPlaneTapped(List<ARHitTestResult> listResults) async {
+    ARHitTestResult hitResult = listResults.firstWhere(
+      (ARHitTestResult hit) => hit.type == ARHitTestResultType.plane,
+    );
+
+    ARPlaneAnchor newAnchor = ARPlaneAnchor(
+      transformation: hitResult.worldTransform,
+    );
+
+    bool? result = await _anchorManager.addAnchor(newAnchor);
+
+    if (result != null && result) {}
   }
 
   toggleLoading() {

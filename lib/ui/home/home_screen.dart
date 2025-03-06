ff169f1e29ffbbd3a51f0aeebcd868a64f77aa45
ricarth.lima <ricarth.lima@gmail.com>
2025-03-06 11:07:30 -0300

    home banner + section widget

--- lib/ui/home/home_screen.dart
+++ lib/ui/home/home_screen.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_ar_android_study/domain/furniture_provider.dart';
 import 'package:flutter_ar_android_study/domain/models/furniture.dart';
+import 'package:flutter_ar_android_study/ui/_core/dimensions.dart';
+import 'package:flutter_ar_android_study/ui/_core/widgets/section_widget.dart';
 import 'package:flutter_ar_android_study/ui/home/widgets/home_app_bar.dart';
 import 'package:flutter_ar_android_study/ui/home/widgets/home_grid_widget.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:provider/provider.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -15,25 +18,44 @@ class HomeScreen extends StatelessWidget {
 
     return Scaffold(
       appBar: getHomeAppBar(context),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: SingleChildScrollView(
-          child: GridView.count(
-            crossAxisCount: 2,
-            childAspectRatio: 3.5 / 10,
-            mainAxisSpacing: 16,
-            crossAxisSpacing: 8,
-            shrinkWrap: true,
-            scrollDirection: Axis.vertical,
-            physics: NeverScrollableScrollPhysics(),
-            children: List.generate(
-              furnitureProvider.listFurniture.length,
-              (index) {
-                Furniture furniture = furnitureProvider.listFurniture[index];
-                return HomeGridWidget(furniture: furniture);
-              },
+      body: SingleChildScrollView(
+        child: Column(
+          spacing: 32,
+          children: [
+            SvgPicture.asset(
+              "assets/home-banner.svg",
+              width: width(context),
             ),
-          ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16),
+              child: Column(
+                children: [
+                  SectionWidget(
+                    title: "Destaques",
+                    child: SingleChildScrollView(
+                      child: GridView.count(
+                        crossAxisCount: 2,
+                        childAspectRatio: 3.5 / 10,
+                        mainAxisSpacing: 16,
+                        crossAxisSpacing: 8,
+                        shrinkWrap: true,
+                        scrollDirection: Axis.vertical,
+                        physics: NeverScrollableScrollPhysics(),
+                        children: List.generate(
+                          furnitureProvider.listFurniture.length,
+                          (index) {
+                            Furniture furniture =
+                                furnitureProvider.listFurniture[index];
+                            return HomeGridWidget(furniture: furniture);
+                          },
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
